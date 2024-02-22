7d5c8432354028b70c55c9a571d9d04eb15086f2
igor3432 <137342044+igor3432@users.noreply.github.com>
2024-02-22 01:26:45 -0300

    processo final do certificado
    certificado fim .

--- app.py
+++ app.py
@@ -9,16 +9,30 @@ for indice,linha in enumerate (sheet_alunos.iter_rows(min_row=2,max_row=2)):
     tipo_participacao = linha[2].value
     data_inicio = linha[3].value
     data_final = linha[4].value
-    carga_Horaria = linha[5].value 
+    carga_horaria = linha[5].value 
     data_emissao = linha[6].value
     
 
     fonte_nome = ImageFont.truetype('./tahoma.ttf',90)
-    fonte_nome = ImageFont.truetype('./tahomabd.ttf',80)
+    fonte_geral = ImageFont.truetype('./tahomabd.ttf',80)
+    fonte_data = ImageFont.truetype('./tahomabd.ttf',55)
 
     image = Image.open('./certificado_padrao.jpg')
     desenhar = ImageDraw.Draw(image)
 
     desenhar.text((1020,827),nome_participante,fill='black',font=fonte_nome)
+
+    desenhar.text((1060,950),nome_curso,fill='black',font=fonte_geral)
+
+    desenhar.text((1435,1065),tipo_participacao,fill='black',font=fonte_geral)
+
+    desenhar.text((1480,1182),str(carga_horaria),fill='black',font=fonte_geral)
+
+    desenhar.text((750,1770),data_inicio,fill='black',font=fonte_data)
+
+    desenhar.text((750,1930),data_final,fill='black',font=fonte_data)
+
+    desenhar.text((2220,1930),data_emissao,fill='black',font=fonte_data)
+    
 
     image.save(f'./(indice)(nome_participante)certificado_padrao.png')
